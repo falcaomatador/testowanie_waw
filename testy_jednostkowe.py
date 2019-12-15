845025c2ec04b2b52e903c304ff990a7a19a9382
Wojtek <falcaomatador@gmail.com>
2019-12-15 13:28:34 +0100

    testy jednostkowe

---
+++ testy_jednostkowe.py
@@ -0,0 +1,27 @@
+# def add(a, b):
+#     return a + b
+#
+#
+# def test_add():
+#     assert add(5, 10) == 15
+#     assert add(7, -1) == 6
+#     assert add(4.3, 5.3) == 9.6
+#
+#
+# test_add()
+
+
+def product(x, y):
+    return x * y
+
+
+def test_product():
+    assert product(3, 3) == 9
+    assert product(12.5, 4) == 50
+    assert product(0, 1) == 0
+
+
+test_product()
+
+
+
