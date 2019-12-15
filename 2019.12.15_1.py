8a6abd3e96193d22e391350bc52c213a9ead2a70
Wojtek <falcaomatador@gmail.com>
2019-12-15 12:37:51 +0100

    Asercje cz. 2

--- 2019.12.15_1.py
+++ 2019.12.15_1.py
@@ -1,9 +1,51 @@
-def div(a, b):
-    return a/b
+# def div(a, b):
+#     return a/b
+#
+#
+# # if div(10, 5) == 2:
+# #     print("Passed")
+# # else:
+# #     raise Exception("FAILED")
+#
+# assert div(10, 5) == 2, "Failed"
+# print("Passed")
+#
+#
+# def sqr_sum(a, b):
+#     return (a + b) ** 2
+#
+#
+# assert sqr_sum(2, 2) == 16, "FAILED"
+# print("PASSED")
+# assert sqr_sum(1, 1) == 4, "FAILED"
+# print("PASSED")
+# assert sqr_sum(3, 3) == 36, "FAILED"
+# print("PASSED")
+# assert sqr_sum(4, 5) == 81, "FAILED"
+# print("PASSED")
 
 
-if div(10, 5) == 2:
-    print("Passed")
-else:
-    print("Failed")
+# asercje nie służą tylko do testowania!
 
+# def div(a, b):
+#    try:
+#         return a/b
+#    except ZeroDivisionError:
+#        print("Nie można dzielić przez zero.")
+
+#
+# def div(a, b):
+#     assert b != 0, "Nie można dzielić przez zero"
+#     return a/b
+#
+#
+# div(5, 0)
+
+
+def concatenate(str1, str2):
+    return str1 + str2
+
+
+assert concatenate("asd", "fgh") == "asdfgh", "Failed"
+assert concatenate("ala", "ma") == "alama", "Failed"
+assert concatenate("", "") == "", "Failed"
